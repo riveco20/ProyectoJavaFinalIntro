7e85efcb4ff77fcef13cbbbd69520107326baeb9
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-12 09:38:26 -0500

    Creando clases para implementaion de los caracterez

---
+++ src/mx/com/gm/proyectojavafinal/implementaciondomain/ImpleOpStr.java
@@ -0,0 +1,4 @@
+package mx.com.gm.proyectojavafinal.implementaciondomain;
+
+public class ImpleOpStr {
+}
---
+++ src/mx/com/gm/proyectojavafinal/implementaciondomain/OperStr.java
@@ -0,0 +1,4 @@
+package mx.com.gm.proyectojavafinal.implementaciondomain;
+
+public interface OperStr {
+}
